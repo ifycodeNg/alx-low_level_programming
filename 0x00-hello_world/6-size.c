fffe70e8de85efed608bfe81f53a16c2507381d2
ifycodeNg <ifyugwumba22@gmail.com>
2022-09-14 15:38:56 -0700

    V10

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,7 +20,6 @@ printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longint));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongInt));
-printf("Size of a double: %zu byte(s)\n", sizeof(doubleType));
 printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
