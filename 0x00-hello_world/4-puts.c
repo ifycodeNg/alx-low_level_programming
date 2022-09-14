81e9177ed897ae3d81d58a65cd3094dc12671528
ifycodeNg <ifyugwumba22@gmail.com>
2022-09-14 09:12:46 -0700

    V4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
-int main()
+int main(void)
 {
-	//string initialisation
-    char Mystr[] = "Programming is like building a multilingual puzzle";
-    
-    puts(Mystr); //writing the string to stdout
-    
-    return 0;
+char Mystr[] = "Programming is like building a multilingual puzzle /n";
+puts(Mystr);
+return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
-int main() {
-    int intType;
-    float floatType;
-    double doubleType;
-    char charType;
-
-    // sizeof evaluates the size of a variable
-    printf("Size of int: %zu bytes\n", sizeof(intType));
-    printf("Size of float: %zu bytes\n", sizeof(floatType));
-    printf("Size of double: %zu bytes\n", sizeof(doubleType));
-    printf("Size of char: %zu byte\n", sizeof(charType));
-    
-    return 0;
+int main(void)
+{
+int intType;
+float floatType;
+double doubleType;
+char charType;
+printf("Size of char: %zu bytes\n", sizeof(charType));
+printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of double: %zu bytes\n", sizeof(doubleType));
+printf("Size of float: %zu byte\n", sizeof(floatType));
+return (0);
 }
