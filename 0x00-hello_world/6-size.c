6947f3d503dedb30ff9e3eac464d71d7d4be2be8
ifycodeNg <ifyugwumba22@gmail.com>
2022-09-14 09:25:07 -0700

    V5

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,22 @@
 #include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 int intType;
 float floatType;
 double doubleType;
 char charType;
-printf("Size of char: %zu bytes\n", sizeof(charType));
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of double: %zu bytes\n", sizeof(doubleType));
-printf("Size of float: %zu byte\n", sizeof(floatType));
+printf("Size of char: %zu byte(s)\n", sizeof(charType));
+printf("Size of int: %zu byte(s)\n", sizeof(intType));
+printf("Size of double: %zu byte(s)\n", sizeof(doubleType));
+printf("Size of float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
