1a4de9ea3ebc96e41ee6d10b351382c968d7fe89
ifycodeNg <ifyugwumba22@gmail.com>
2022-09-14 10:02:31 -0700

    V6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-char Mystr[] = "Programming is like building a multilingual puzzle /n";
+char Mystr[] = "Programming is like building a multilingual puzzle \n";
 puts(Mystr);
 return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,12 @@ int intType;
 float floatType;
 double doubleType;
 char charType;
+long long int longlongInt;
+long int longint
 printf("Size of char: %zu byte(s)\n", sizeof(charType));
 printf("Size of int: %zu byte(s)\n", sizeof(intType));
+printf("Size of long int: %zu byte(s)\n", sizeof(longint));
+printf("Size of long long int: %zu byte(s)\n", sizeof(longlongint));
 printf("Size of double: %zu byte(s)\n", sizeof(doubleType));
 printf("Size of float: %zu byte(s)\n", sizeof(floatType));
 return (0);
